be8b9ff0067a5ac08d72d63064567602be22d312
nayaksneha07482 <nayaksneha07482@gmail.com>
2023-02-21 22:43:29 +0530

    plan div js

--- scripts/index.js
+++ scripts/index.js
@@ -53,3 +53,57 @@ cards.map(item => {
     cardBox.append(imageDiv, heading, para);
     document.querySelector("#cardsContainer").append(cardBox);
 });
+
+// Plan div cards
+
+
+var planCards = [
+    {
+        image: "https://assets-global.website-files.com/5d03b4e130118314af624b20/6192cc9d75bbb20e8074e602_Rectangle%20108.jpg",
+        savings: "",
+        number: "4-5 Meals",
+    },
+    {
+        image: "https://assets-global.website-files.com/5d03b4e130118314af624b20/6192cc9d5fc8ef6e6127d841_Rectangle%20108-1.jpg",
+        savings: "Save up to $12",
+        number: "6-7 Meals",
+    },
+    {
+        image: "https://assets-global.website-files.com/5d03b4e130118314af624b20/6192cc9c24f1e287b2ea6a19_Rectangle%20108-2.jpg",
+        savings: "Save up to $20",
+        number: "8-9 Meals",
+    },
+    {
+        image: "https://assets-global.website-files.com/5d03b4e130118314af624b20/6192cc9d58c6a6a96a2ad5ef_Rectangle%20108-3.jpg",
+        savings: "Save up to $27",
+        number: "10-11 Meals",
+    },
+    {
+        image: "https://assets-global.website-files.com/5d03b4e130118314af624b20/6192cc9ccb70a3f88391e136_Rectangle%20108-4.jpg",
+        savings: "Save up to $33",
+        number: "12 Meals",
+    },
+];
+
+planCards.map(item => {
+    var box = document.createElement("div");
+
+    var image = document.createElement("img");
+    image.setAttribute("src", item.image);
+
+    if (item.savings != "") {
+        var saving = document.createElement("p");
+        saving.textContent = item.savings;
+    }
+    else {
+        var saving = document.createElement("p");
+        saving.textContent = item.savings;
+        saving.style.display = "none"
+    }
+
+    var heading = document.createElement("h4");
+    heading.textContent = item.number;
+
+    box.append(image, saving, heading);
+    document.querySelector(".planContainer").append(box);
+});
